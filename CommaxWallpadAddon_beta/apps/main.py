6ecbe6f9a344934aed2cd1b19714eab856f3840c
woooook <nubiz@naver.com>
2025-01-03 16:02:35 +0900

    9

--- CommaxWallpadAddon_beta/apps/main.py
+++ CommaxWallpadAddon_beta/apps/main.py
@@ -293,7 +293,8 @@ class WallpadController:
             device_count = {name: 0 for name in state_headers.values()}
             
             # COLLECTDATA의 recv_data 분석
-            for data in self.COLLECTDATA['recv_data']:
+            collect_data_set = set(self.COLLECTDATA['recv_data'])
+            for data in collect_data_set:
                 data_bytes = bytes.fromhex(data)
                 header = byte_to_hex_str(data_bytes[0])
                 if data == checksum(data) and header in state_headers:
@@ -309,6 +310,7 @@ class WallpadController:
                         self.logger.debug(f'기기 갯수 업데이트: {device_count[name]}')
                     except:
                         #header가 존재하지만 deviceId가 없는 경우 1개로 처리
+                        self.logger.debug(f'deviceId가 없는 기기: {name}')
                         device_count[name] = 1
             
             # 검색 결과 처리
