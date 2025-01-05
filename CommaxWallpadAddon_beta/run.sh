256aae70124d0f943477b5323e02065a51252ac6
woooook <nubiz@naver.com>
2025-01-05 10:41:22 +0900

    5

--- CommaxWallpadAddon_beta/run.sh
+++ CommaxWallpadAddon_beta/run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/with-contenv bashio
+MQTT_HOST=$(bashio::services mqtt "host")
+MQTT_USER=$(bashio::services mqtt "username")
+MQTT_PASSWORD=$(bashio::services mqtt "password")
+
+echo "MQTT_HOST: $MQTT_HOST"
+echo "MQTT_USER: $MQTT_USER"
+echo "MQTT_PASSWORD: $MQTT_PASSWORD"
 
 python3 -m apps.main
